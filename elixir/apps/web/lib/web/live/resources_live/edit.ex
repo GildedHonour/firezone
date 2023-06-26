7315f78ebcd0b2009d671f7f5b067c5bb240605f
Jamil Bou Kheir <jamilbk@users.noreply.github.com>
2023-06-25 19:18:47 -0700

    Finish stubbing out Policy CRUD

--- elixir/apps/web/lib/web/live/resources_live/edit.ex
+++ elixir/apps/web/lib/web/live/resources_live/edit.ex
@@ -22,7 +22,7 @@ defmodule Web.ResourcesLive.Edit do
         Edit Resource
       </:title>
     </.section_header>
-    <!-- Update Resource -->
+    <!-- Edit Resource -->
     <section class="bg-white dark:bg-gray-900">
       <div class="max-w-2xl px-4 py-8 mx-auto lg:py-16">
         <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Edit Resource details</h2>
--- elixir/apps/web/lib/web/live/resources_live/show.ex
+++ elixir/apps/web/lib/web/live/resources_live/show.ex
@@ -25,7 +25,7 @@ defmodule Web.ResourcesLive.Show do
         </.edit_button>
       </:actions>
     </.section_header>
-    <!-- Gateway details -->
+    <!-- Resource details -->
     <div class="bg-white dark:bg-gray-800 overflow-hidden">
       <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <tbody>
@@ -82,7 +82,7 @@ defmodule Web.ResourcesLive.Show do
         </tbody>
       </table>
     </div>
-    <!-- Linked Resources table -->
+    <!-- Linked Gateways table -->
     <div class="grid grid-cols-1 p-4 xl:grid-cols-3 xl:gap-4 dark:bg-gray-900">
       <div class="col-span-full mb-4 xl:mb-2">
         <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
